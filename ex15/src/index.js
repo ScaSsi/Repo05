56385df4434184072453bc12f5ca31fc77d061d3
Avujcic1 <avujcic1@etf.unsa.ba>
2022-04-01 15:55:53 +0200

    World blanks

---
+++ ex15/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "I saw a " + myAdjective + " " + myNoun + " who " + myVerb + " very " + myAdverb + ".";
+    // Only change code above this line
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
